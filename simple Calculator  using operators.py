340a7751a9fe8e3da31893b40c71d943ad05dc5e
Hanumanthumanoj <82634361+Hanumanthumanoj@users.noreply.github.com>
2021-07-01 10:33:09 +0530

    Update simple Calculator  using operators.py

--- simple Calculator  using operators.py
+++ simple Calculator  using operators.py
@@ -1,7 +1,7 @@
 print("****Simple Calculator***",sep=" ")
 a=int(input("Enter the numbere1"))
 b=int(input("Enter the number2"))
-c=input("Operators for calculator is + , - , * , / , // , < , > , == , != ")
+c=input("Operators for calculator is + , - , * , / , // ")
 if(c=="+"):
     print(f"{a+b}")
 elif(c=="-"):
@@ -12,17 +12,5 @@ elif(c=="/"):
     print(f"{a/b}")
 elif(c=="//"):
     print(f"{a//b}")
-elif(c=="<"):
-    if(a<b):
-        print(b,"Is greatest")
-elif(c==">"):
-    if(a>b):
-        print(a,"Is Greatest")
-elif(c==("==")):
-    if(a==b):
-        print("Equal Values")
-elif(c==("!=")):
-    if(a!=b):
-        print("Both values are not Equal")
 else:
     pass
